0bd2e1fe33d81dc256fcc3b5ebe4c6686927ac6f
dancers <149972831+alluding@users.noreply.github.com>
2024-05-18 09:10:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,49 +1,43 @@
-from setuptools import setup
+from typing import List, Dict, Optional
+
+from setuptools import setup, find_packages
+import subprocess
 import re
 
-#requirements = []
-#with open('requirements.txt') as f:
-#    requirements = f.read().splitlines()
+def get_version() -> str:
+    version: str = '2.4.4'
+    if not version:
+        raise RuntimeError('version is not set')
 
-version = '2.4.4'
-if not version:
-    raise RuntimeError('version is not set')
+    if version.endswith(('a', 'b', 'rc')):
+        try:
+            commit_count = subprocess.check_output(['git', 'rev-list', '--count', 'HEAD']).decode('utf-8').strip()
+            version += commit_count
+            commit_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('utf-8').strip()
+            version += '+g' + commit_hash
+        except subprocess.CalledProcessError:
+    return version
 
-if version.endswith(('a', 'b', 'rc')):
-    # append version identifier based on commit count
+def read_readme() -> str:
     try:
-        import subprocess
-
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += '+g' + out.decode('utf-8').strip()
+        with open('README.rst', 'r', encoding='utf-8') as f:
+            return f.read()
     except Exception:
-        pass
+        return ''
 
-readme = ''
-try:
-    with open('README.rst') as f:
-        readme = f.read()
-except: pass
-
-extras_require = {
+extras_require: Dict[str, List[str]] = {
     'voice': ['PyNaCl>=1.3.0,<1.6'],
     'docs': [
         'sphinx==4.4.0',
         'sphinxcontrib_trio==1.1.2',
         'sphinxcontrib-websupport',
-        'typing-extensions>=4.3,<5',
+        'typing_extensions>=4.3,<5',
     ],
     'speed': [
         'orjson>=3.5.4',
         'aiodns>=1.1',
         'Brotli',
-        'cchardet==2.1.7; python_version < "3.10"',
+        f'cchardet==2.1.7; python_version < "3.10"',
     ],
     'test': [
         'coverage[toml]',
@@ -51,51 +45,46 @@ extras_require = {
         'pytest-asyncio',
         'pytest-cov',
         'pytest-mock',
-        'typing-extensions>=4.3,<5',
+        'typing_extensions>=4.3,<5',
     ],
 }
 
-packages = [
-    'discord',
-    'discord.types',
-    'discord.ui',
-    'discord.webhook',
-    'discord.app_commands',
-    'discord.ext.commands',
-    'discord.ext.tasks',
-]
+def setup_package() -> None:
+    setup(
+        name='alluding',
+        author='cop-discord',
+        url='https://github.com/alluding/disdick',
+        project_urls={
+            'Documentation': 'https://discordpy.readthedocs.io/en/latest/',
+            'Issue tracker': 'https://github.com/alluding/disdick/issues',
+        },
+        version=get_version(),
+        packages=find_packages(),
+        license='MIT',
+        description='A Python wrapper for the Discord API, forked from disdick - discord.py.',
+        long_description=read_readme(),
+        long_description_content_type='text/x-rst',
+        include_package_data=True,
+        install_requires=[
+            'aiohttp', 'aiodns', 'orjson', 'typing_extensions', 'psutil', 'durations_nlp', 'fast_string_match'
+        ],
+        extras_require=extras_require,
+        python_requires='>=3.8.0',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: MIT License',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Topic :: Internet',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Utilities',
+            'Typing :: Typed',
+        ],
+    )
 
-setup(
-    name='disdick',
-    author='cop-discord',
-    url='https://github.com/alluding/disdick',
-    project_urls={
-        'Documentation': 'https://discordpy.readthedocs.io/en/latest/',
-        'Issue tracker': 'https://github.com/alluding/disdick/issues',
-    },
-    version=version,
-    packages=packages,
-    license='MIT',
-    description='A Python wrapper for the Discord API, forked from disdick - discord.py.',
-    long_description=readme,
-    long_description_content_type='text/x-rst',
-    include_package_data=True,
-    install_requires=['aiohttp','aiodns','orjson','typing_extensions','psutil','durations_nlp','fast_string_match'],
-    extras_require=extras_require
-    python_requires='>=3.8.0',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        'Typing :: Typed',
-    ],
-)
+setup_package()
